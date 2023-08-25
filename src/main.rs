81a70bb53739b6cdb35ca6cd9e00941536ae2090
strict-flower <126295255+strict-flower@users.noreply.github.com>
2023-08-26 00:57:15 +0900

    table of contents

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
+use chrono::offset::{FixedOffset, Utc};
+use chrono::DateTime;
 use comrak::ComrakOptions;
+use serde::Serialize;
 use std::cmp::{Ord, Ordering, PartialOrd};
-use std::fs::read_dir;
+use std::fs::{read_dir, Metadata};
 use std::path::{Path, PathBuf};
 
 type SsgResult<T> = Result<T, Box<dyn std::error::Error>>;
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Serialize)]
 enum PageNode {
     IndexPage(PathBuf, Vec<PageNode>),
-    Article(PathBuf),
+    Article(PathBuf, String, String),
 }
 
 impl PartialOrd for PageNode {
@@ -20,21 +23,27 @@ impl PartialOrd for PageNode {
                     o => Some(o),
                 }
             }
-            (&PageNode::IndexPage(path1, _), &PageNode::Article(path2)) => {
+            (&PageNode::IndexPage(path1, _), &PageNode::Article(path2, _, _)) => {
                 if path1 == path2 {
                     None
                 } else {
                     Some(Ordering::Greater) // an article must be less than index page
                 }
             }
-            (&PageNode::Article(path1), &PageNode::IndexPage(path2, _)) => {
+            (&PageNode::Article(path1, _, _), &PageNode::IndexPage(path2, _)) => {
                 if path1 == path2 {
                     None
                 } else {
                     Some(Ordering::Less) // an article must be less than index page
                 }
             }
-            (&PageNode::Article(path1), &PageNode::Article(path2)) => Some(path1.cmp(path2)),
+            (
+                &PageNode::Article(_, created_at1, modified_at1),
+                &PageNode::Article(_, created_at2, modified_at2),
+            ) => Some(match created_at1.cmp(created_at2) {
+                Ordering::Equal => modified_at1.cmp(modified_at2),
+                o => o,
+            }),
         }
     }
 }
@@ -71,11 +80,23 @@ impl Ssg {
             src,
             dest,
             option,
-            template: String::from_utf8(std::fs::read(template_file)?)?,
+            template: std::fs::read_to_string(template_file)?,
         })
     }
 
-    fn process_file(&self, file: PathBuf, base_path: PathBuf) -> SsgResult<PageNode> {
+    fn process_file(
+        &self,
+        file: PathBuf,
+        base_path: PathBuf,
+        metadata: Metadata,
+    ) -> SsgResult<PageNode> {
+        let title = file
+            .file_name()
+            .unwrap()
+            .to_str()
+            .unwrap()
+            .strip_suffix(".md")
+            .unwrap();
         let relative_path = file.strip_prefix(&self.src)?;
         let relative_path = relative_path.strip_prefix(&base_path)?;
         let url = base_path.join(relative_path);
@@ -87,13 +108,27 @@ impl Ssg {
             url,
             dest_path.to_str().unwrap()
         );
-        let markdown = String::from_utf8(std::fs::read(&file)?)?;
-        let html = self.template.replace(
-            "{{ article }}",
-            &comrak::markdown_to_html(&markdown, &self.option),
-        );
+        let markdown = std::fs::read_to_string(&file)?;
+        let html = comrak::markdown_to_html(&markdown, &self.option);
+        let html = self.template.replace("{{ article }}", &html);
+        let html = html.replace("{{ title }}", title);
+
+        let created_at: DateTime<Utc> = metadata.created()?.into();
+        let created_at = created_at
+            .with_timezone(&FixedOffset::east_opt(32400).unwrap())
+            .format("%Y/%m/%d %H:%I")
+            .to_string();
+
+        let modified_at: DateTime<Utc> = metadata.modified()?.into();
+        let modified_at = modified_at
+            .with_timezone(&FixedOffset::east_opt(32400).unwrap())
+            .format("%Y/%m/%d %H:%I")
+            .to_string();
+
+        let html = html.replace("{{ created_at }}", &created_at);
+        let html = html.replace("{{ modified_at }}", &modified_at);
         std::fs::write(dest_path.as_path(), html.into_bytes())?;
-        Ok(PageNode::Article(url.into()))
+        Ok(PageNode::Article(url.into(), created_at, modified_at))
     }
 
     fn process(&self, current: PathBuf) -> SsgResult<PageNode> {
@@ -106,7 +141,7 @@ impl Ssg {
             let fname = fname.to_str().unwrap();
             let ftype = entry.file_type()?;
             if ftype.is_file() && fname.ends_with(".md") {
-                res.push(self.process_file(entry.path(), current.clone())?);
+                res.push(self.process_file(entry.path(), current.clone(), entry.metadata()?)?);
             } else if ftype.is_dir() {
                 let next_cur = current.join(fname);
                 res.push(self.process(next_cur)?);
@@ -127,8 +162,17 @@ fn main() -> SsgResult<()> {
     }
     let source_dir = PathBuf::from(&args[1]);
     let destination_dir = PathBuf::from(&args[2]);
-    let template_file = Path::new("assets/template.html");
-    let ssg = Ssg::new(source_dir, destination_dir, template_file)?;
-    dbg!(ssg.process(PathBuf::new())?);
+    let template_file = Path::new("assets/template_article.html");
+    let index_template_file = Path::new("assets/template_index.html");
+    let ssg = Ssg::new(source_dir, destination_dir.clone(), template_file)?;
+    let index = ssg.process(PathBuf::from(""))?;
+    let index_template = std::fs::read_to_string(index_template_file)?;
+    let index_html = index_template.replace("{{ data }}", &serde_json::to_string(&index)?);
+
+    std::fs::write(
+        destination_dir.join("index.html").as_path(),
+        index_html.into_bytes(),
+    )?;
+
     Ok(())
 }
